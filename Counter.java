f23dcede2170e3b187bb310cb2bfb25b1c1a5a95
jcxxxddd <97922708+jcxxxddd@users.noreply.github.com>
2022-02-03 16:03:54 -0800

    Lab 3 - Letter Inventory

--- Counter.java
+++ Counter.java
@@ -1,14 +1,47 @@
 public class Counter{
-    public int value;
-    public String letter;
 
-    public Counter(int newValue, String newLetter){
-        value = newValue;
+    private char letter;
+    private int occurences;
+    
+    public Counter(char newLetter, int newOccurences)
+    {
         letter = newLetter;
-
+        occurences = newOccurences;    
     }
-    public char getLetter(){
-        return letter.charAt(0); //converts letter from a string to a character.
 
+    public Counter(int charIndex, int startingOccurences)
+    {
+        this((char)charIndex, startingOccurences);
     }
+
+    public Counter(Counter counter)
+    {
+        this(counter.letter, counter.occurences);
+    }
+
+    public void addToValue(int valueToAdd)
+    {
+        occurences += valueToAdd;
+    }
+
+    public int getOccurences()
+    {
+        return occurences;
+    }
+
+    public void setOccurences(int value)
+    {
+        occurences = value;
+    }
+
+    public char getLetter()
+    {
+        return letter;
+    }
+
+    public void setLetter(char character)
+    {
+        letter = character;
+    }
+
 }
--- LetterInventory.java
+++ LetterInventory.java
@@ -1,86 +1,167 @@
-import java.util.ArrayList;
-
 public class LetterInventory{
 
-    public static void main(String[] args){
+    // Joel Fletcher
+    // cs145 parsons
+    public static void main(String[] args)
+    {
         System.out.println("test");
-        LetterInventory li = new LetterInventory("");
+        LetterInventory li = new LetterInventory("hellllK]Hlllo");
         System.out.println(li.toString());
-        System.out.println(li.size());//counts all ocurences of all letters
-        //System.out.println(li.get('l'));
-        //li.set('l', 6);
-        //System.out.println(li.get('l'));
-        System.out.println(li.size());
-        System.out.println(li.isEmpty());
+        System.out.println("Number of l is: " + li.get('l'));
+        System.out.println("Number of L is: " + li.get('L'));
     }
-    
-    public ArrayList<Counter> inventory = new ArrayList<Counter>();
 
-    public LetterInventory (String input){
-        inventory = new ArrayList<Counter>();
-        for(int i=0; i < input.length(); i++){//will run through each char in the string
-            boolean found = false;
-            for(int j = 0; j < inventory.size(); j++){
-                if(String.valueOf(input.charAt(i)).equals(inventory.get(j).letter)){
-                    inventory.get(j).value ++;
-                    found = true;
-                }
-            }
-            if (!found){
-                inventory.add(new Counter(1, String.valueOf(input.charAt(i))));
-            }
-        }   
+    // Array of counters to hold the letters and occurence total
+    private Counter[] inventory;
+
+    // Default size (26 letters) and ascii starting index (97) to grab lowercase letters
+    private static final int inventorySize = 26;
+    private static final int alphabetStartingIndex = 97;
+
+    // keeps track of total size of inventory
+    private int totalCharacterCount = 0;
+
+    // Default Constructor creates new empty inventory with all chars and 0 occurences
+    public LetterInventory(){
+        inventory = new Counter[inventorySize];
+        for(int i = 0; i < inventorySize; i++)
+            inventory[i] = new Counter(i+alphabetStartingIndex,0);
     }
-    
-    public int get(char letter){
-        var occurences = 0;
-        for( int i = 0; i < inventory.size(); i++){
-            if(letter == (inventory.get(i).getLetter())){
-                occurences = inventory.get(i).value;
-            }
+
+    // Primary construct takes in a string and creates inventory based on that
+    public LetterInventory (String input)
+    {
+        // Call default constructor to initialize array
+        this();
+
+        // Convert string to all lowercase to avoid case issues
+        input = input.toLowerCase();
+
+        // Run through each character in the string and try to add to inventory
+        for(int i=0; i < input.length(); i++)
+            try{
+                addOccurrenceOfLetter(input.charAt(i));}
+            catch(IllegalArgumentException e){
+                System.out.println("Unable to add all characters from string: " + e.getMessage());}
+    }
+
+    // Outputs a simple string representation of our inventory
+    public String toString()
+    {
+        // Wrap output with square brackets
+        String output = "[";
+
+        // Add the letter to output string equal to the number of occurences
+        for(Counter counter: inventory)
+            for(int i = 0; i < counter.getOccurences(); i++)
+                output += counter.getLetter();
+
+        return output + "]";
+    }
+
+    // Gets the number of occurences of this letter in the inventory
+    public int get(char letter)
+    {
+        // Convert char to lowercase 
+        letter = Character.toLowerCase(letter);
+
+        // Get index by subracting out the starting index of the character in ascii table ('a' starts at 65)
+        int counterIndex = letter - alphabetStartingIndex;
+
+        // Check if it's now a valid index for our inventory, if it return occurence total if not throw excep
+        if(validIndex(counterIndex))
+            return inventory[counterIndex].getOccurences();
+        else
+            throw new IllegalArgumentException("Unable to find letter: '" + letter + "'" + ", has ascii index: " + (int)letter);
+    }
+
+    // Sets the number of occurences of this letter in the inventory to given value
+    public void set(char letter, int value)
+    {
+        // Convert char to lowercase 
+        letter = Character.toLowerCase(letter);
+        
+        // Get index by subracting out the starting index of the character in ascii table ('a' starts at 65)
+        int counterIndex = letter - alphabetStartingIndex;
+
+        // Check if it's now a valid index for our inventory, if it is modify occurence total
+        if(validIndex(counterIndex))
+        {
+            totalCharacterCount += (value - inventory[counterIndex].getOccurences());
+            inventory[counterIndex].setOccurences(value);
         }
-        return occurences;
-
+        else
+            throw new IllegalArgumentException("Unable to find letter: '" + letter + "'" + ", has ascii index: " + (int)letter);
     }
 
-    public void set(char letter, int value){
-        for( int i = 0; i < inventory.size(); i++){
-            if(letter == (inventory.get(i).getLetter())){
-                inventory.get(i).value = value;
-            }
-        }
+    // Outputs a new inventory equal to the sum of current inventory and the passed one
+    public LetterInventory add(LetterInventory letterInventory)
+    {
+        // Create new LetterInventory
+        LetterInventory output = new LetterInventory();
 
-    }
-    
-    public String toString(){
-        String output = "";
-        for(int i = 0; i < inventory.size(); i ++){
-            for(int j = 0; j < inventory.get(i).value; j++){
-                output += inventory.get(i).letter; //adds letter to string output = to nunmber of times of its value
-
-            }
-
+        // Add the occurences from both inventories into new one
+        for(int i = alphabetStartingIndex; i < inventorySize + alphabetStartingIndex; i++)
+        {
+            output.addOccurrenceOfLetter((char)i, get((char)i) + letterInventory.get((char)i));
         }
 
         return output;
     }
-    
-    public int size(){
-        int totalCount = 0;
-        for(int i = 0; i < inventory.size(); i ++){
-            totalCount += inventory.get(i).value;
+
+    // Outputs a new inventory equal to the difference of current inventory and the passed one
+    public LetterInventory subtract(LetterInventory letterInventory)
+    {
+        // Create new LetterInventory
+        LetterInventory output = new LetterInventory();
+
+        // Add the occurences from both inventories into new one
+        for(int i = alphabetStartingIndex; i < inventorySize + alphabetStartingIndex; i++)
+        {
+            output.addOccurrenceOfLetter((char)i, get((char)i) - letterInventory.get((char)i));
+            if(output.get((char)(i)) < 0)
+                return null;
         }
 
-        return totalCount;
+        return output;
     }
-   
+
+    // Returns the sum of all counts in inventory
+    public int size(){
+        return totalCharacterCount;
+    }
+
+    // Returns whether inventory is empty or not
     public boolean isEmpty(){
-        boolean empty = true;
-        for(int i = 0; i < inventory.size(); i ++){
-            if(inventory.get(i).value > 0){
-                return false;
-            }
-        }
-        return empty;
+        return totalCharacterCount == 0;
+    }
+
+
+    ////////////////////////////////
+    //// Custom Helper Functions ///
+    ////////////////////////////////
+
+    // Attempts to add a character to our inventory, throws exception if invalid character
+    public void addOccurrenceOfLetter(char letter)
+    {
+        addOccurrenceOfLetter(letter,1);
+    }
+
+    // Attempts to add a character to our inventory, throws exception if invalid character
+    public void addOccurrenceOfLetter(char letter, int occurences)
+    {
+        set(letter, get(letter) + occurences);
+    }
+
+    // Helper function to easily convert from integer to character based on ascii number
+    public int get(int asciiNumber)
+    {
+        return get((char)(asciiNumber));
+    }
+
+    // Helper function to make sure we are getting counter from a valid index in inventory
+    public boolean validIndex(int index)
+    {
+        return (index >=0 && index < 26);
     }
 }
